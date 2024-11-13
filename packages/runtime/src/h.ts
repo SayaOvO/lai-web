33c752c6da0d795d03887d6abbcc89dd28397862
linjie <sayaOvO@pm.me>
2024-11-13 15:33:29 +0800

    feat:(h.ts, utils/arrays.ts): VDOM creation API

---
+++ packages/runtime/src/h.ts
@@ -0,0 +1,87 @@
+import { withoutNulls } from './utils/arrays'
+
+export enum VDOM_TYPE {
+  TEXT,
+  ELEMENT,
+  FRAGMENT,
+}
+
+export interface TEXTVNode {
+  type: VDOM_TYPE.TEXT
+  value: string
+  el?: Text
+}
+
+export interface ElementVNode {
+  type: VDOM_TYPE.ELEMENT
+  tag: string
+  props: ElementVNodeProps
+  children: VNode[]
+  el?: HTMLElement
+}
+
+export interface FragmentVNode {
+  type: VDOM_TYPE.FRAGMENT
+  children: VNode[]
+  el?: HTMLElement
+}
+
+interface ElementVNodeProps {
+  on?: { [key: string]: EventListener }
+  [key: string]: unknown
+}
+
+type VNode = TEXTVNode | ElementVNode | FragmentVNode
+
+export function hString(
+  value: string | number | symbol | bigint | boolean
+): TEXTVNode {
+  return {
+    type: VDOM_TYPE.TEXT,
+    value: String(value),
+  }
+}
+
+export function h(
+  tag: string,
+  props: ElementVNodeProps = {},
+  children: ChildNodeType[] = []
+): ElementVNode {
+  return {
+    type: VDOM_TYPE.ELEMENT,
+    tag,
+    props,
+    children: mapPrimitiveToText(withoutNulls(children)),
+  }
+}
+
+export function hFragment(children: ChildNodeType[] = []) {
+  return {
+    type: VDOM_TYPE.FRAGMENT,
+    children: mapPrimitiveToText(withoutNulls(children)), // null or undefined will render nothing
+  }
+}
+
+export type ChildNodeType =
+  | VNode
+  | null
+  | undefined
+  | string
+  | number
+  | symbol
+  | bigint
+  | boolean
+
+function mapPrimitiveToText(
+  children: Exclude<ChildNodeType, null | undefined>[]
+) {
+  return children.map((child) =>
+    typeof child === 'string' ||
+    typeof child === 'number' ||
+    typeof child === 'boolean' ||
+    typeof child === 'bigint' ||
+    typeof child === 'symbol'
+      ? hString(child)
+      : child
+  )
+}
