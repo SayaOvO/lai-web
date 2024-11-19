c3f4173da9ccf3c35f9cecbb3b17f2797ecfcda3
Saya <sayaOvO@pm.me>
2024-11-19 08:55:29 +0800

    feat(component): updateState

--- packages/runtime/src/__tests__/component.test.ts
+++ packages/runtime/src/__tests__/component.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, expect, test, vi, describe } from 'vitest'
 import { defineComponent, Component } from '../component'
-import { h } from '../h'
+import { h, hFragment } from '../h'
 
 beforeEach(() => {
   vi.unstubAllGlobals()
@@ -47,6 +47,14 @@ describe('a stateful component', () => {
     expect(document.body.innerHTML).toBe('<button>0</button>')
   })
 
+  test('unmount', () => {
+    const btn = new Button()
+    btn.mount(document.body)
+    expect(document.body.innerHTML).toBe('<button>0</button>')
+    btn.unmount()
+    expect(document.body.innerHTML).toBe('')
+  })
+
   test('component with props', () => {
     type Props = { initialState: number }
     type State = { count: number }
@@ -65,5 +73,50 @@ describe('a stateful component', () => {
       initialState: 2,
     })
     expect(btn.render()).toEqual(h('button', {}, [2]))
+  })
+
+  test('component return fragment', () => {
+    const Comp = defineComponent({
+      render() {
+        return hFragment([
+          h('button', {}, ['button one']),
+          h('button', {}, ['button two']),
+        ])
+      },
+    })
+    document.body.append(document.createElement('p'))
+    const cmp = new Comp()
+    cmp.mount(document.body)
+    expect(document.body.innerHTML).toEqual(
+      '<p></p><button>button one</button><button>button two</button>'
+    )
+  })
+})
+
+describe('updateState', () => {
+  test('', () => {
+    type Props = { initialState: number }
+    type State = { count: number }
+    const Button = defineComponent<Props, State>({
+      render() {
+        return h('button', {}, [this.state.count])
+      },
+      state(props?: { initialState: number }) {
+        return {
+          count: props?.initialState ?? 0,
+        }
+      },
+    })
+
+    const btn = new Button({
+      initialState: 2,
+    })
+    expect(btn.render()).toEqual(h('button', {}, [2]))
+    btn.mount(document.body)
+    btn.updateState({
+      count: 5,
+    })
+    expect(btn.render()).toEqual(h('button', {}, [5]))
+    expect(document.body.innerHTML).toEqual('<button>5</button>')
   })
 })
--- packages/runtime/src/component.ts
+++ packages/runtime/src/component.ts
@@ -1,5 +1,7 @@
+import { destroyDOM } from './destroy-dom'
 import { VNode, h } from './h'
 import { mountDOM } from './mount-dom'
+import { patchDOM } from './patch-dom'
 
 interface ComponentOptions<Props, State = {}> {
   render: (this: Component<Props, State>) => VNode
@@ -10,6 +12,8 @@ export interface Component<Props, State = {}> {
   render: (this: Component<Props, State>) => VNode
   mount: (parentEl: HTMLElement, index?: number | null) => void
   state: State
+  unmount: () => void
+  updateState: (newState: Partial<State>) => void
 }
 
 export function defineComponent<Props, State = {}>({
@@ -19,6 +23,7 @@ export function defineComponent<Props, State = {}>({
   props?: Props
 ) => Component<Props, State> {
   class UserComponent implements Component<Props, State> {
+    #hostEl: HTMLElement | null = null
     #isMounted = false
     vdom: VNode | null = null
     state: State = {} as State
@@ -26,19 +31,47 @@ export function defineComponent<Props, State = {}>({
     constructor(props?: Props) {
       this.state = state ? state(props) : ({} as State)
     }
-
     render(): VNode {
       return render.call(this)
+    }
+
+    updateState(state: Partial<State>) {
+      this.state = { ...this.state, ...state }
+      this.#patch()
+    }
+
+    // rerender
+    #patch() {
+      if (!this.vdom || !this.#hostEl) {
+        return
+      }
+      const newVdom = this.render()
+      console.log('newVdom:', newVdom)
+      this.vdom = patchDOM(this.vdom, newVdom, this.#hostEl)
     }
 
     mount(parentEl: HTMLElement, index: number | null = null) {
       if (this.#isMounted) {
         throw new Error('The component has already mounted')
       }
+      this.#hostEl = parentEl
       this.vdom = this.render()
 
       mountDOM(this.vdom, parentEl, index)
       this.#isMounted = true
+    }
+
+    unmount() {
+      if (!this.#isMounted) {
+        throw new Error('The component is not mounted.')
+      }
+      if (this.vdom) {
+        // there are more need to do
+        destroyDOM(this.vdom)
+        this.vdom = null
+        this.#isMounted = false
+        this.state = {} as State
+      }
     }
   }
 
